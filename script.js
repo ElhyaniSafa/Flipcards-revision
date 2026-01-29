96db0f1f93a99b807830442cd1dbe3caeaa6462d
SAFA EL HYANI <elhyanisafa@gmail.com>
2026-01-29 14:47:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+var state1 = false;
+var state2 = false;
+var state3 = false;
+var state4 = false;
+var state5 = false;
+var state6 = false;
+
+
+function flip1() {
+  var q = document.getElementById("q1");
+  var a = document.getElementById("a1");
+
+  if (state1 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state1 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state1 = false;
+  }
+}
+
+function flip2() {
+  var q = document.getElementById("q2");
+  var a = document.getElementById("a2");
+
+  if (state2 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state2 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state2 = false;
+  }
+}
+
+function flip3() {
+  var q = document.getElementById("q3");
+  var a = document.getElementById("a3");
+
+  if (state1 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state1 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state1 = false;
+  }
+}
+
+function flip4() {
+  var q = document.getElementById("q4");
+  var a = document.getElementById("a4");
+
+  if (state1 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state1 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state1 = false;
+  }
+}
+
+function flip5() {
+  var q = document.getElementById("q5");
+  var a = document.getElementById("a5");
+
+  if (state1 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state1 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state1 = false;
+  }
+}
+
+function flip6() {
+  var q = document.getElementById("q6");
+  var a = document.getElementById("a6");
+
+  if (state1 === false) {
+    q.style.display = "none";
+    a.style.display = "block";
+    state1 = true;
+  } else {
+    q.style.display = "block";
+    a.style.display = "none";
+    state1 = false;
+  }
+}
